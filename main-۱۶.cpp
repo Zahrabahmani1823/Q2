1b23b3165d589499aaa15e97a4eab7309c8742c4
Zahrabahmani1823 <Zahrabahmani4175@gmail.com>
2024-06-13 03:51:45 +0330

    Add files via upload

---
+++ main-۱۶.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+ int i;
+ cout<<"please enter a number"<<endl;
+ cin>>i;
+ for(int a=1;a<=i;a++){
+ cout<<"*";
+ }
+ return 0;
+ }
